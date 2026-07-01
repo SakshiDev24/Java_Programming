3f146080ce6e998cb1a96fc7449bad0a04249d01
Sakshi Jagtap <sakshijagtap20052005@gmail.com>
2026-07-01 21:52:24 +0530

    Add files via upload

---
+++ GUI1.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+
+
+class GUI1
+{
+    public static void main(String A[])
+    {
+        JFrame fobj = new JFrame("marvellous PPA");
+
+        fobj.setSize(400,300);
+
+        fobj.setVisible(true);
+
+    }
+}
---
+++ GUI2.java
@@ -0,0 +1,15 @@
+import java.awt.*;
+
+
+class GUI2
+{
+    public static void main(String A[])
+    {
+        Frame fobj = new Frame("marvellous PPA");
+
+        fobj.setSize(400,300);
+
+        fobj.setVisible(true);
+
+    }
+}
